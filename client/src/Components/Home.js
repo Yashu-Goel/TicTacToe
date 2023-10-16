81a73c1f9e16252a3ff1449613a64db41f04f5a3
Yashu <yashugoeldpsi@gmail.com>
2023-10-16 15:36:47 +0530

    added star shower effect

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,49 +1,51 @@
-import React, { useState } from "react";
-import "./Home.css";
-import X from "../Assets/X.png"
-import O from "../Assets/O.png"
-const Home = () => {
-  const [uniqueCode, setUniqueCode] = useState("");
+  import React, { useState } from "react";
+  import "./Home.css";
+  import X from "../Assets/X.png"
+  import O from "../Assets/O.png"
+  import ShootingStars from "./ShootingStars";
+  const Home = () => {
+    const [uniqueCode, setUniqueCode] = useState("");
 
-  const generateUniqueCode = () => {
-    const characters =
-      "0123456789";
-    const codeLength = 6;
+    const generateUniqueCode = () => {
+      const characters =
+        "0123456789";
+      const codeLength = 6;
 
-    let generatedCode = "";
-    for (let i = 0; i < codeLength; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      generatedCode += characters.charAt(randomIndex);
-    }
+      let generatedCode = "";
+      for (let i = 0; i < codeLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        generatedCode += characters.charAt(randomIndex);
+      }
 
-    setUniqueCode(generatedCode);
-  };
+      setUniqueCode(generatedCode);
+    };
 
-  const handlePlayWithRandom = () => {
-    console.log("Playing with a random person");
-  };
+    const handlePlayWithRandom = () => {
+      console.log("Playing with a random person");
+    };
 
-  const handlePlayWithFriend = () => {
-    generateUniqueCode();
-  };
+    const handlePlayWithFriend = () => {
+      generateUniqueCode();
+    };
 
-  return (
-    <div className="HomeOuterContainer">
-      <img className="XImage" src={X} />
-      <img className="OImage" src={O} />
-      <div className="HomeInnerContainer">
-        <div className="HomeButtons">
-          <button onClick={handlePlayWithRandom}>
-            Play with Random Person
-          </button>
+    return (
+      <div className="HomeOuterContainer">
+        <img className="XImage" src={X} />
+        <img className="OImage" src={O} />
+        <div className="HomeInnerContainer">
+          <div className="HomeButtons">
+            <button onClick={handlePlayWithRandom}>
+              Play with Random Person
+            </button>
+          </div>
+          <div className="HomeButtons">
+            <button onClick={handlePlayWithFriend}>Play with Friend</button>
+            {uniqueCode && <p>Your Unique Code: {uniqueCode}</p>}
+          </div>
         </div>
-        <div className="HomeButtons">
-          <button onClick={handlePlayWithFriend}>Play with Friend</button>
-          {uniqueCode && <p>Your Unique Code: {uniqueCode}</p>}
-        </div>
+        <ShootingStars/>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Home;
+  export default Home;
---
+++ client/src/Components/ShootingStars.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ShootingStars.css"; // Create a CSS file for the shooting stars
+
+const ShootingStars = () => {
+  return (
+    <div className="shooting-stars-container">
+      <section className="ShootingInner">
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+        <span className="ShootingSpan"></span>
+      </section>
+    </div>
+  );
+};
+
+export default ShootingStars;
